b258d8d46fa51e1acb4a3a4e542ef187879e21e5
alif adwitiya pratama <alifadwitiyap@gmail.com>
2022-06-15 13:38:43 +0700

    first commit

---
+++ app.py
@@ -0,0 +1,48 @@
+import pandas as pd
+from figure import *
+from bokeh.models.widgets import Tabs, Panel
+from bokeh.io import curdoc
+
+
+# Preprocess
+df = pd.read_csv(
+    './data/covid_19_indonesia_time_series_all.csv', parse_dates=['Date'])
+pd.to_datetime(df['Date'])
+df = df[['Date', 'Location', 'New Cases', 'New Deaths', 'New Recovered']].rename(
+    columns={'New Cases': 'NewCases', 'New Deaths': 'NewDeaths', 'New Recovered': 'NewRecovered'})
+
+LOCATIONLIST = df.Location.unique().tolist()
+
+
+# It creates a dropdown menu
+dropdown = Select(title="Location:", value=LOCATIONLIST[1],
+                  options=LOCATIONLIST)
+
+# A callback function. It is called when the value of the dropdown menu is changed.
+
+
+def dropdownUpdate(attr, old, new):
+    source.data = df[df.Location == dropdown.value]
+
+
+dropdown.on_change('value', dropdownUpdate)
+
+# It creates a date range slider with the given parameters.
+dateSlider = DateRangeSlider(
+    title="Date Range:", start=df.Date.min(), end=df.Date.max(), value=(df.Date.min(), df.Date.max())
+)
+
+# build source data
+source = ColumnDataSource(df[df.Location == LOCATIONLIST[1]])
+
+# It adds the layout to the current document.
+l1 = newCaseFigure(source, dropdown, dateSlider)
+l2 = newRecoveredFigure(source, dropdown, dateSlider)
+l3 = newDeathFigure(source, dropdown, dateSlider)
+
+tab1 = Panel(child=l1, title="New Cases")
+tab2 = Panel(child=l2, title="New Recovered")
+tab3 = Panel(child=l3, title="New Death")
+tabs = Tabs(tabs=[tab1, tab2, tab3])
+
+curdoc().add_root(tabs)
---
+++ figure.py
@@ -0,0 +1,137 @@
+from bokeh.plotting import figure
+from bokeh.layouts import column
+from bokeh.models import ColumnDataSource, HoverTool, CustomJS
+from bokeh.models.widgets import Select, DateRangeSlider
+
+
+def newCaseFigure(source, dropdown, dateSlider):
+
+    # list tools
+    TOOLS = "pan,wheel_zoom,reset"
+
+    # set hover
+    HOVER = HoverTool(
+        tooltips=[
+            ('Date',   '@Date{%F}'),
+            ('New Case', '@NewCases'),
+        ],
+
+        formatters={
+            '@Date': 'datetime',
+        },
+
+        mode='vline'
+    )
+
+    # Creating a figure object with the given parameters.
+    figureNewCase = figure(title="New Case Covid",
+                           plot_height=500, plot_width=900,
+                           x_axis_label="Tanggal", y_axis_label="New Case", x_axis_type="datetime", tools=TOOLS)
+
+    # It adds the hover tool to the figure.
+    figureNewCase.add_tools(HOVER)
+
+    # It plots the line graph of the data in the source.
+    figureNewCase.line('Date', 'NewCases',
+                       source=source)
+
+    # A callback function. It is called when the value of the date range slider is changed.
+    callback = CustomJS(args=dict(p=figureNewCase), code="""
+        console.log(p)
+        p.x_range.start = cb_obj.value[0]
+        p.x_range.end = cb_obj.value[1]
+        p.x_range.change.emit()
+        """)
+
+    dateSlider.js_on_change('value_throttled', callback)
+
+    return column(figureNewCase, dropdown, dateSlider)
+
+
+def newDeathFigure(source, dropdown, dateSlider):
+
+    # list unique location
+    # list tools
+    TOOLS = "pan,wheel_zoom,reset"
+
+    # set hover
+    HOVER = HoverTool(
+        tooltips=[
+            ('Date',   '@Date{%F}'),
+            ('New Death', '@NewDeaths'),
+        ],
+
+        formatters={
+            '@Date': 'datetime',
+        },
+
+        mode='vline'
+    )
+
+    # Creating a figure object with the given parameters.
+    figureNewDeaths = figure(title="New Death",
+                             plot_height=500, plot_width=900,
+                             x_axis_label="Tanggal", y_axis_label="Death", x_axis_type="datetime", tools=TOOLS)
+
+    # It adds the hover tool to the figure.
+    figureNewDeaths.add_tools(HOVER)
+
+    # It plots the line graph of the data in the source.
+    figureNewDeaths.line('Date', 'NewDeaths',
+                         source=source)
+
+    # A callback function. It is called when the value of the date range slider is changed.
+    callback = CustomJS(args=dict(p=figureNewDeaths), code="""
+        console.log(p)
+        p.x_range.start = cb_obj.value[0]
+        p.x_range.end = cb_obj.value[1]
+        p.x_range.change.emit()
+        """)
+
+    dateSlider.js_on_change('value_throttled', callback)
+
+    return column(figureNewDeaths, dropdown, dateSlider)
+
+
+def newRecoveredFigure(source, dropdown, dateSlider):
+
+    # list tools
+    TOOLS = "pan,wheel_zoom,reset"
+
+    # set hover
+    HOVER = HoverTool(
+        tooltips=[
+            ('Date',   '@Date{%F}'),
+            ('New Recovered', '@NewRecovered'),
+        ],
+
+        formatters={
+            '@Date': 'datetime',
+        },
+
+        mode='vline'
+    )
+
+    # Creating a figure object with the given parameters.
+    figureNewRecovered = figure(title="New Recovered",
+                                plot_height=500, plot_width=900,
+                                x_axis_label="Tanggal", y_axis_label="Recovered", x_axis_type="datetime", tools=TOOLS)
+
+    # It adds the hover tool to the figure.
+    figureNewRecovered.add_tools(HOVER)
+
+    # It plots the line graph of the data in the source.
+    figureNewRecovered.line('Date', 'NewRecovered',
+                            source=source)
+
+    # A callback function. It is called when the value of the date range slider is changed.
+    callback = CustomJS(args=dict(p=figureNewRecovered), code="""
+        console.log(p)
+        p.x_range.start = cb_obj.value[0]
+        p.x_range.end = cb_obj.value[1]
+        p.x_range.change.emit()
+        """)
+
+    dateSlider.js_on_change('value_throttled', callback)
+
+    return column(figureNewRecovered, dropdown, dateSlider)
