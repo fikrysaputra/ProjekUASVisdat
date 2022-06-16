12e2d89f089c6dffeb1d86b4c78ec5aa79f1276f
Fikry Saputra <77012767+fikrysaputra@users.noreply.github.com>
2022-06-16 23:36:30 +0700

    Update app.py

--- app.py
+++ app.py
@@ -10,21 +10,15 @@ df = pd.read_csv(
 pd.to_datetime(df['Date'])
 df = df[['Date', 'Location', 'New Cases', 'New Deaths', 'New Recovered']].rename(
     columns={'New Cases': 'NewCases', 'New Deaths': 'NewDeaths', 'New Recovered': 'NewRecovered'})
-
 LOCATIONLIST = df.Location.unique().tolist()
-
 
 # It creates a dropdown menu
 dropdown = Select(title="Location:", value=LOCATIONLIST[1],
                   options=LOCATIONLIST)
 
 # A callback function. It is called when the value of the dropdown menu is changed.
-
-
 def dropdownUpdate(attr, old, new):
     source.data = df[df.Location == dropdown.value]
-
-
 dropdown.on_change('value', dropdownUpdate)
 
 # It creates a date range slider with the given parameters.
@@ -39,10 +33,12 @@ source = ColumnDataSource(df[df.Location == LOCATIONLIST[1]])
 l1 = newCaseFigure(source, dropdown, dateSlider)
 l2 = newRecoveredFigure(source, dropdown, dateSlider)
 l3 = newDeathFigure(source, dropdown, dateSlider)
+l4 = anggotaKelompok()
 
-tab1 = Panel(child=l1, title="New Cases")
-tab2 = Panel(child=l2, title="New Recovered")
-tab3 = Panel(child=l3, title="New Death")
-tabs = Tabs(tabs=[tab1, tab2, tab3])
+tab1 = Panel(child=l4, title="Anggota")
+tab2 = Panel(child=l1, title="New Cases")
+tab3 = Panel(child=l2, title="New Recovered")
+tab4 = Panel(child=l3, title="New Death")
+tabs = Tabs(tabs=[tab1, tab2, tab3, tab4])
 
 curdoc().add_root(tabs)
